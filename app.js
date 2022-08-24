d8efc873143ec5a54aa29edd782a23729589cbd5
AlexMagar <lx.mgr07@gmail.com>
2022-08-24 10:23:33 +1000

    handle post request and send item to list.ejs

--- app.js
+++ app.js
@@ -4,36 +4,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 //creating constant using express()
 const app = express();
 app.set('view engine', 'ejs'); //use ejs as its view engine, it has to place below the new app const
 
+app.use(bodyParser.urlencoded({extended: true})); //using bodyParser
+
 //create get route that sends the browser text, when user tris access the home route
 app.get("/", function(req, res){
+
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options ={ weekday: 'long', year:'numeric', month:'long', day:'numeric'};
 
-    switch (currentDay) {
-        case 0: day = "Sunday";            
-            break;
-        case 1: day = "Monday";            
-            break;
-        case 2: day = "Tuesday";            
-            break;
-        case 3: day = "Wednesday";            
-            break;
-        case 4: day = "Thursday";            
-            break;
-        case 5: day = "Friday";            
-            break;
-        case 6: day = "Saturday";            
-            break;    
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }   
+    var day = today.toLocaleDateString("en-GB", options);
 
-    res.render("list", {KindOfDay: day})
+    res.render("list", {KindOfDay: day, newListItems: items});
+});
+
+//handle post request form index.html, / means home route which is app.js
+app.post("/",function(req, res){
+
+    //we need to use body parser inorder to use value of body
+    var item = req.body.newItem; //newItem is name of input type in index.html
+    
+    items.push(item); //pusing item in array list
+    res.redirect("/") // redirect to home route 
 });
 
 //listen in port 3000
